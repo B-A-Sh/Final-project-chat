1d00d0b2f072353343fc1e46050360cd2a31802d
B-A-Sh <baramos19@gmail.com>
2025-01-07 17:53:36 +0200

    last touch after merging, inventation win

---
+++ client/socketChat/src/pages/chat components/Chatbox/gameInventationWin.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const gameInventationWin = () => {
+  return (
+    <div className='gameInventationWin'>
+        <div className='gameButton'>
+            <img src="" alt="memoryGame" />
+        </div>
+        
+
+    </div>
+  )
+}
+
+export default gameInventationWin
